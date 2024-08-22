3225a7889c9acdfd5591ab3ebdce118ac47af15c
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-22 22:15:59 +0200

    Create a webpage with a 16x16 grid of square divs
    Created script that generate grid of squares.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const container = document.querySelector("container");
+let tableSize = 80;
+const container = document.querySelector(".container");
+
+for (j = 0; j < tableSize; j++) {
+    const rowElement = document.createElement("div")
+    rowElement.classList.add("row");
+    for (i = 0; i < tableSize; i++) {
+        const squareElement = document.createElement("div")
+        squareElement.classList.add("square");
+        rowElement.appendChild(squareElement);
+    }
+    container.appendChild(rowElement);
+}
