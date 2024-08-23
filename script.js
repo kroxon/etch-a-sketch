296f3bb9822aae9dfe742ac099646074309e87d9
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-08-23 14:24:37 +0200

    Manage drawing
    Created function that change background color from white to black depend on how many times event is triggered.

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-let tableSize = 80;
+let tableSize = 20;
 const container = document.querySelector(".container");
 
 for (j = 0; j < tableSize; j++) {
     const rowElement = document.createElement("div")
     rowElement.classList.add("row");
     for (i = 0; i < tableSize; i++) {
-        const squareElement = document.createElement("div")
+        let squareElement = document.createElement("div")
         squareElement.classList.add("square");
         squareElement.addEventListener("mouseover", () => {
-            squareElement.addEventListener('mousedown', function () {
-                alert("Hello World");
-            });
+            if (trigger == true) {
+                decrementBackground(squareElement);
+            }
         });
         rowElement.appendChild(squareElement);
     }
     container.appendChild(rowElement);
+}
+
+// check if mouse is clicked
+let trigger = false;
+document.addEventListener('mousedown', function () {
+    trigger = true;
+});
+
+document.addEventListener('mouseup', function () {
+    trigger = false;
+});
+
+function decrementBackground(square) {
+    let squareColor = window.getComputedStyle(square).backgroundColor;
+    console.log(squareColor);
+    const endIndex = squareColor.indexOf(',');
+    let colorLevel = parseInt(squareColor.slice(4, endIndex));
+    colorLevel = (colorLevel > 10) ? colorLevel - 30 : 0;
+    square.style.backgroundColor = `rgb(${colorLevel}, ${colorLevel}, ${colorLevel})`;
 }
